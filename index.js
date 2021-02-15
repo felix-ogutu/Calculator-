c4cf1c929607c6d51bae8db54f7ed29555372b08
felix-ogutu <61490127+felix-ogutu@users.noreply.github.com>
2021-02-15 12:08:18 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from "./Calculator";
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Calculator/>
+  </React.StrictMode>,
+  document.getElementById('wrapper')
+);
+
+
